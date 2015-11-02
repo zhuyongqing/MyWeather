fc7de519bdd4250f856e93dba9ed3ab5d91a86bf
zhuyongqing <372910103@qq.com>
2015-11-02 16:39:06 +0800

    Initial Commit

---
+++ MyWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyWeather
+//
+//  Created by zhuyongqing on 15/11/2.
+//  Copyright © 2015年 zhuyongqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
